474845dac00ecc79fb07dcc37996214ed137249b
Zaid <zaidsyed7021@gmail.com>
2026-06-20 19:11:52 +0530

    auto: update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -114,6 +114,7 @@ export default function App() {
           <EventFeed />
           <CouncilChamber />
           <Timeline />
+          <StatsPanel />
         </aside>
       </main>
     </div>
