75d5a3b9f984d1fe1dc9d2e4da9683915ff9746d
Zaid <zaidsyed7021@gmail.com>
2026-06-22 22:40:06 +0530

    auto: update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -139,6 +139,7 @@ export default function App() {
           </span>
         ))}
         <span className="grow" />
+        <StabilityBadge />
         <SpeedControl />
         <SpendCounter />
       </header>
